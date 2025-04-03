0993386d6c002e2c7f10e0422929939b41d7fba1
kuznetsov-251 <anton.kuznetsov.is21b@mail.ru>
2025-04-03 12:37:58 +0300

    CLOSE #10. Добавление пагинации на форму "Сотрудники". CLOSE #11. Условное форматирование на форме "Госпитализации".

--- Hospital/Hospital/EmployeesForm.Designer.cs
+++ Hospital/Hospital/EmployeesForm.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EmployeesForm));
             this.dataGridViewEmployees = new System.Windows.Forms.DataGridView();
             this.searchTextBox = new System.Windows.Forms.RichTextBox();
             this.roleFilterComboBox = new System.Windows.Forms.ComboBox();
+            this.labRowCount = new System.Windows.Forms.Label();
+            this.buttonNextPage = new System.Windows.Forms.Button();
+            this.buttonPreviousPage = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmployees)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,7 +53,7 @@
             this.dataGridViewEmployees.BackgroundColor = System.Drawing.Color.White;
             this.dataGridViewEmployees.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.dataGridViewEmployees.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewEmployees.Location = new System.Drawing.Point(-6, -4);
+            this.dataGridViewEmployees.Location = new System.Drawing.Point(11, 54);
             this.dataGridViewEmployees.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.dataGridViewEmployees.MultiSelect = false;
             this.dataGridViewEmployees.Name = "dataGridViewEmployees";
@@ -58,7 +65,7 @@
             // 
             // searchTextBox
             // 
-            this.searchTextBox.Location = new System.Drawing.Point(2, 506);
+            this.searchTextBox.Location = new System.Drawing.Point(11, 10);
             this.searchTextBox.MaxLength = 30;
             this.searchTextBox.Multiline = false;
             this.searchTextBox.Name = "searchTextBox";
@@ -67,23 +74,94 @@
             this.searchTextBox.Text = "";
             this.searchTextBox.WordWrap = false;
             this.searchTextBox.TextChanged += new System.EventHandler(this.SearchTextBox_TextChanged);
+            this.searchTextBox.Enter += new System.EventHandler(this.searchTextBox_Enter);
+            this.searchTextBox.Leave += new System.EventHandler(this.searchTextBox_Leave);
             // 
             // roleFilterComboBox
             // 
             this.roleFilterComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.roleFilterComboBox.FormattingEnabled = true;
-            this.roleFilterComboBox.Location = new System.Drawing.Point(618, 506);
+            this.roleFilterComboBox.Location = new System.Drawing.Point(627, 10);
             this.roleFilterComboBox.Name = "roleFilterComboBox";
             this.roleFilterComboBox.Size = new System.Drawing.Size(382, 32);
             this.roleFilterComboBox.TabIndex = 2;
             this.roleFilterComboBox.SelectedIndexChanged += new System.EventHandler(this.RoleFilterComboBox_SelectedIndexChanged);
+            // 
+            // labRowCount
+            // 
+            this.labRowCount.AutoSize = true;
+            this.labRowCount.Location = new System.Drawing.Point(7, 566);
+            this.labRowCount.Name = "labRowCount";
+            this.labRowCount.Size = new System.Drawing.Size(197, 24);
+            this.labRowCount.TabIndex = 3;
+            this.labRowCount.Text = "Количество записей: ";
+            // 
+            // buttonNextPage
+            // 
+            this.buttonNextPage.Image = ((System.Drawing.Image)(resources.GetObject("buttonNextPage.Image")));
+            this.buttonNextPage.Location = new System.Drawing.Point(171, 590);
+            this.buttonNextPage.MaximumSize = new System.Drawing.Size(33, 33);
+            this.buttonNextPage.MinimumSize = new System.Drawing.Size(33, 33);
+            this.buttonNextPage.Name = "buttonNextPage";
+            this.buttonNextPage.Size = new System.Drawing.Size(33, 33);
+            this.buttonNextPage.TabIndex = 18;
+            this.buttonNextPage.UseVisualStyleBackColor = true;
+            this.buttonNextPage.Click += new System.EventHandler(this.buttonNextPage_Click);
+            // 
+            // buttonPreviousPage
+            // 
+            this.buttonPreviousPage.BackColor = System.Drawing.SystemColors.Window;
+            this.buttonPreviousPage.Image = ((System.Drawing.Image)(resources.GetObject("buttonPreviousPage.Image")));
+            this.buttonPreviousPage.Location = new System.Drawing.Point(12, 590);
+            this.buttonPreviousPage.Name = "buttonPreviousPage";
+            this.buttonPreviousPage.Size = new System.Drawing.Size(33, 33);
+            this.buttonPreviousPage.TabIndex = 17;
+            this.buttonPreviousPage.UseVisualStyleBackColor = false;
+            this.buttonPreviousPage.Click += new System.EventHandler(this.buttonPreviousPage_Click);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutoPopDelay = 10000;
+            this.toolTip1.BackColor = System.Drawing.Color.PowderBlue;
+            this.toolTip1.ForeColor = System.Drawing.Color.Black;
+            this.toolTip1.InitialDelay = 500;
+            this.toolTip1.ReshowDelay = 100;
+            this.toolTip1.ShowAlways = true;
+            this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.toolTip1.ToolTipTitle = "Помощь";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1182, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 24);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "?";
+            this.toolTip1.SetToolTip(this.label1, resources.GetString("label1.ToolTip"));
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.PowderBlue;
+            this.button1.Location = new System.Drawing.Point(1027, 583);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 33);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Назад";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // EmployeesForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1180, 543);
+            this.ClientSize = new System.Drawing.Size(1212, 628);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonNextPage);
+            this.Controls.Add(this.buttonPreviousPage);
+            this.Controls.Add(this.labRowCount);
             this.Controls.Add(this.roleFilterComboBox);
             this.Controls.Add(this.searchTextBox);
             this.Controls.Add(this.dataGridViewEmployees);
@@ -94,11 +172,12 @@
             this.MinimizeBox = false;
             this.Name = "EmployeesForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Управление сотрудниками";
+            this.Text = "Сотрудники";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.EmployeesForm_FormClosed);
             this.Load += new System.EventHandler(this.EmployeesForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmployees)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -107,5 +186,12 @@
         private System.Windows.Forms.DataGridView dataGridViewEmployees;
         private System.Windows.Forms.RichTextBox searchTextBox;
         private System.Windows.Forms.ComboBox roleFilterComboBox;
+        private System.Windows.Forms.Label labRowCount;
+        private System.Windows.Forms.Button buttonNextPage;
+        private System.Windows.Forms.Button buttonPreviousPage;
+        private System.Windows.Forms.ToolTip toolTip2;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Hospital/Hospital/EmployeesForm.cs
+++ Hospital/Hospital/EmployeesForm.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using MySql.Data.MySqlClient;
+using ToolTip = System.Windows.Forms.ToolTip;
 
 namespace Hospital
 {
@@ -19,22 +20,37 @@ namespace Hospital
         private DataView dt; // Представление данных для фильтрации
         private ContextMenuStrip employeeContextMenu; // Контекстное меню
 
+
+        private int selectedRowIndex = -1;
+        private int currentNumPage = 0;
+
         public EmployeesForm()
         {
             InitializeComponent(); // Инициализация компонентов
-            InitializeContextMenu(); // Настройка контекстного меню
-            LoadEmployeeData(); // Загрузка данных о сотрудниках
-            LoadRoles(); // Загрузка ролей
         }
 
         private void InitializeContextMenu()
         {
             employeeContextMenu = new ContextMenuStrip(); // Создание контекстного меню
             ToolStripMenuItem addEmployeeMenuItem = new ToolStripMenuItem("Добавить сотрудника"); // Пункт "Добавить"
+            ToolStripMenuItem removeEmployeeMenuItem = new ToolStripMenuItem("Удалить сотрудника"); // Пункт "Добавить"
             employeeContextMenu.Items.Add(addEmployeeMenuItem);
+            employeeContextMenu.Items.Add(removeEmployeeMenuItem);
             addEmployeeMenuItem.Click += AddEmployeeMenuItem_Click; // Обработчик добавления
+            removeEmployeeMenuItem.Click += RemoveEmployeeMenuItem_Click;
 
             dataGridViewEmployees.ContextMenuStrip = employeeContextMenu;
+        }
+
+        private void RemoveEmployeeMenuItem_Click(object sender, EventArgs e)
+        {
+            if (selectedRowIndex != -1)
+                DeleteEmployee(selectedRowIndex);
+            else
+            {
+                MessageBox.Show("Не выбрана запись для удаления.", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
         }
 
         private void LoadEmployeeData()
@@ -74,12 +90,10 @@ namespace Hospital
                         Role.HeaderText = "Роль";
                         dataGridViewEmployees.Columns.Add(Role);
 
-                        DataGridViewButtonColumn deleteButtonColumn = new DataGridViewButtonColumn(); // Колонка удаления
-                        deleteButtonColumn.Name = "DeleteColumn";
-                        deleteButtonColumn.Text = "Удалить";
-                        deleteButtonColumn.HeaderText = "Удаление";
-                        deleteButtonColumn.UseColumnTextForButtonValue = true;
-                        dataGridViewEmployees.Columns.Add(deleteButtonColumn);
+                        DataGridViewTextBoxColumn Login = new DataGridViewTextBoxColumn();
+                        Login.DataPropertyName = "Login";
+                        Login.HeaderText = "Логин";
+                        dataGridViewEmployees.Columns.Add(Login);
 
                         dt = bedTable.DefaultView; // Установка представления данных
                         dataGridViewEmployees.DataSource = dt; // Привязка данных
@@ -92,11 +106,160 @@ namespace Hospital
             catch (Exception ex)
             {
                 MessageBox.Show($"{ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
             }
+        }
+
+        void Pagination()
+        {
+            dataGridViewEmployees.CurrentCell = null;
+            foreach (DataGridViewRow r in dataGridViewEmployees.Rows)
+                r.Visible = false;
+
+            // Узнаём сколько страниц будет
+            int pageSize = 20; // Записей на странице
+            int totalRows = dataGridViewEmployees.Rows.Count;
+            int totalPages = (int)Math.Ceiling((double)totalRows / pageSize); // Используем Math.Ceiling для округления вверх
+
+            // Обновляем UI (удаляем старые элементы пагинации)
+            ClearPaginationUI();
+
+            // Создаем LinkLabel для страниц
+            LinkLabel[] ll = new LinkLabel[totalPages];
+            int x = buttonPreviousPage.Location.X + 38;
+            int y = 593, step = 15;
+
+            for (int i = 0; i < totalPages; ++i)
+            {
+                ll[i] = new LinkLabel();
+                ll[i].Text = Convert.ToString(i + 1);
+                ll[i].Name = "page" + i;
+                ll[i].AutoSize = true;
+                ll[i].Location = new Point(x, y);
+                ll[i].Click += new EventHandler(LinkLabel_Click);
+                this.Controls.Add(ll[i]);
+
+                x += step;
+            }
+
+            // Устанавливаем активную страницу (подчеркивание)
+            SetActivePage(currentNumPage, ll);
+
+            labRowCount.Text = "Общее количество записей: " + totalRows;
+
+            buttonNextPage.Location = totalPages > 0 ? new Point(ll[totalPages - 1].Location.X + 25, 593) : new Point(x, y); //Обработка если нет данных
+            UpdateButtonState();
+            DisplayData(currentNumPage, pageSize);
+        }
+
+        private void ClearPaginationUI()
+        {
+            // Удаляем LinkLabel служащие для пагинации
+            List<Control> controlsToRemove = new List<Control>();
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is LinkLabel && ctrl.Name.StartsWith("page"))
+                {
+                    controlsToRemove.Add(ctrl);
+                }
+            }
+            foreach (Control ctrl in controlsToRemove)
+            {
+                this.Controls.Remove(ctrl);
+            }
+        }
+
+        private void SetActivePage(int pageNum, LinkLabel[] ll)
+        {
+            // Сбрасываем подчеркивание у всех LinkLabel
+            foreach (var ctrl in this.Controls)
+            {
+                if (ctrl is LinkLabel)
+                {
+                    (ctrl as LinkLabel).LinkBehavior = LinkBehavior.AlwaysUnderline;
+                }
+            }
+
+            // Убираем подчеркивание у активной страницы, если она существует
+            if (ll != null && ll.Length > pageNum && pageNum >= 0)
+            {
+                ll[pageNum].LinkBehavior = LinkBehavior.NeverUnderline;
+            }
+        }
+
+        // выбор страницы пагинации
+        // те строки которые нам не нужны на выбраной странице - скрываем
+        private void LinkLabel_Click(object sender, EventArgs e)
+        {
+            dataGridViewEmployees.ClearSelection();
+            dataGridViewEmployees.CurrentCell = null;
+
+            // узнаём какая страница выбрана
+            LinkLabel l = sender as LinkLabel;
+            if (l != null)
+            {
+                currentNumPage = Convert.ToInt32(l.Text) - 1;
+                Pagination(); //Перерисовываем интерфейс
+            }
+        }
+
+        private void buttonPreviousPage_Click(object sender, EventArgs e)
+        {
+            if (currentNumPage > 0)
+            {
+                currentNumPage--;
+                Pagination();
+            }
+        }
+
+        private void buttonNextPage_Click(object sender, EventArgs e)
+        {
+            int pageSize = 20;
+            int totalRows = dataGridViewEmployees.Rows.Count;
+            int totalPages = (int)Math.Ceiling((double)totalRows / pageSize);
+
+            if (currentNumPage < totalPages - 1)
+            {
+                currentNumPage++;
+                Pagination();
+            }
+        }
+
+        private void DisplayData(int pageNum, int pageSize)
+        {
+            int start = pageNum * pageSize;
+            int end = Math.Min(start + pageSize, dataGridViewEmployees.Rows.Count); //Используем Math.Min на случай, если записей меньше, чем pageSize
+
+            // Сначала показываем все строки, чтобы избежать проблем при переключении страниц
+            foreach (DataGridViewRow row in dataGridViewEmployees.Rows)
+            {
+                row.Visible = true;
+            }
+
+            // Скрываем строки, которые не входят в текущую страницу
+            for (int i = 0; i < dataGridViewEmployees.Rows.Count; i++)
+            {
+                dataGridViewEmployees.Rows[i].Visible = (i >= start && i < end);
+            }
+
+            labRowCount.Text = "Показаны записи: " + (start + 1) + " - " + end + " из " + dataGridViewEmployees.Rows.Count; //Для красоты добавил +1 к start
+
+            dataGridViewEmployees.Refresh();
+            UpdateButtonState();
+        }
+        private void UpdateButtonState()
+        {
+            int pageSize = 20;
+            int totalRows = dataGridViewEmployees.Rows.Count;
+            int totalPages = (int)Math.Ceiling((double)totalRows / pageSize);
+
+            buttonPreviousPage.Enabled = (currentNumPage > 0);
+            buttonNextPage.Enabled = (currentNumPage < totalPages - 1);
         }
 
         private void LoadRoles()
         {
+            roleFilterComboBox.DataSource = null;
             using (MySqlConnection connection = new MySqlConnection(GlobalValue.GetConnString()))
             {
                 try
@@ -109,7 +272,7 @@ namespace Hospital
 
                     DataRow allRolesRow = roleTable.NewRow(); // Добавление строки "Все роли"
                     allRolesRow["RoleID"] = 0;
-                    allRolesRow["RoleName"] = "Все роли";
+                    allRolesRow["RoleName"] = "Фильтрация по роли сотрудника";
                     roleTable.Rows.InsertAt(allRolesRow, 0);
 
                     roleFilterComboBox.DataSource = roleTable; // Привязка данных к ComboBox
@@ -119,6 +282,7 @@ namespace Hospital
                 catch (Exception ex)
                 {
                     MessageBox.Show("Ошибка загрузки comboBox: " + ex.Message);
+                    return;
                 }
             }
         }
@@ -156,30 +320,57 @@ namespace Hospital
 
         private void SearchTextBox_TextChanged(object sender, EventArgs e)
         {
+            if (searchTextBox.Text == "Начните ввод для поиска по фио сотрудника...")
+                return;
+
+            if (searchTextBox.Text.Length == 0)
+                return;
+            // Сначала показываем все строки, чтобы избежать проблем при переключении страниц
+            foreach (DataGridViewRow row in dataGridViewEmployees.Rows)
+            {
+                row.Visible = true;
+            }
+
             string filter = $"EmployeeFIO LIKE '%{searchTextBox.Text}%'"; // Фильтр по ФИО
             if (roleFilterComboBox.SelectedValue != null && roleFilterComboBox.SelectedIndex != 0)
             {
                 filter += $" AND RoleName = '{roleFilterComboBox.SelectedValue}'"; // Фильтр по роли
             }
             dt.RowFilter = filter; // Применение фильтра
+
+            dataGridViewEmployees.Refresh(); // Обновление DataGridView
+
+            Pagination();
+            currentNumPage = 0;
         }
 
         private void RoleFilterComboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
+            // Сначала показываем все строки, чтобы избежать проблем при переключении страниц
+            foreach (DataGridViewRow row in dataGridViewEmployees.Rows)
+            {
+                row.Visible = true;
+            }
+
             string filter = "";
             if (roleFilterComboBox.SelectedValue != null && roleFilterComboBox.SelectedIndex != 0)
             {
                 filter = $"RoleName = '{roleFilterComboBox.SelectedValue}'"; // Фильтр по роли
             }
-            if (!string.IsNullOrEmpty(searchTextBox.Text))
+            if (!string.IsNullOrEmpty(searchTextBox.Text) && searchTextBox.Text != "Начните ввод для поиска по фио сотрудника...")
             {
                 if (!string.IsNullOrEmpty(filter))
                 {
                     filter += " AND ";
                 }
-                filter += $"EmployeeSurname LIKE '%{searchTextBox.Text}%'"; // Фильтр по фамилии
+                filter += $"EmployeeFIO LIKE '%{searchTextBox.Text}%'"; // Фильтр по фамилии
             }
             dt.RowFilter = filter; // Применение фильтра
+
+            dataGridViewEmployees.Refresh(); // Обновление DataGridView
+
+            Pagination();
+            currentNumPage = 0;
         }
 
         private void DataGridViewEmployees_CellClick(object sender, DataGridViewCellEventArgs e)
@@ -187,42 +378,47 @@ namespace Hospital
             if (e.RowIndex >= 0)
             {
                 dataGridViewEmployees.Rows[e.RowIndex].Selected = true; // Выбор строки
-            }
-
-            if (e.ColumnIndex == dataGridViewEmployees.Columns["DeleteColumn"].Index && e.RowIndex >= 0)
-            {
-                if (MessageBox.Show("Вы уверены, что хотите удалить выбранную учетную запись?", "Подтверждение удаления", MessageBoxButtons.YesNo) == DialogResult.Yes)
-                {
-                    DeleteEmployee(e.RowIndex); // Удаление сотрудника
-                }
+                selectedRowIndex = e.RowIndex;
             }
         }
 
         private void DeleteEmployee(int rowIndex)
         {
-            DataRowView rowToDelete = (DataRowView)dataGridViewEmployees.Rows[rowIndex].DataBoundItem;
-            if (rowToDelete != null)
+            int employeeID = (int)dataGridViewEmployees.Rows[rowIndex].Cells[0].Value; // Получение ID сотрудника
+
+            using (MySqlConnection connection = new MySqlConnection(GlobalValue.GetConnString()))
             {
-                int employeeID = (int)rowToDelete["EmployeeID"]; // Получение ID сотрудника
-
-                using (MySqlConnection connection = new MySqlConnection(GlobalValue.GetConnString()))
+                try
                 {
-                    try
-                    {
-                        connection.Open(); // Открытие соединения
-                        string query = "DELETE FROM employee WHERE EmployeeID = @EmployeeID";
-                        MySqlCommand command = new MySqlCommand(query, connection);
-                        command.Parameters.AddWithValue("@EmployeeID", employeeID);
-                        command.ExecuteNonQuery(); // Удаление сотрудника
+                    connection.Open(); // Открытие соединения
+                    string query = "DELETE FROM employee WHERE EmployeeID = @EmployeeID";
+                    MySqlCommand command = new MySqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@EmployeeID", employeeID);
+                    command.ExecuteNonQuery(); // Удаление сотрудника
 
-                        employeeTable.Rows.Remove(rowToDelete.Row); // Удаление строки из таблицы
-                        employeeTable.AcceptChanges();
-                        MessageBox.Show("Учетная запись сотрудника удалена.", "Успешно", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("Учетная запись сотрудника удалена.", "Успешно", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    LoadEmployeeData();
+                    Pagination();
+
+                    return;
+                }
+                catch (MySqlException ex)
+                {
+                    if (ex.Number == 1451)
+                    {
+                        MessageBox.Show("Ошибка удаления:\n Данная учетная запись имеет связанные записи. Её удаление повлечет за собой утрату многих записей. Удаление невозможно.");
+                        return;
                     }
-                    catch (Exception ex)
+                    else
                     {
                         MessageBox.Show("Ошибка удаления: " + ex.Message);
+                        return;
                     }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка удаления: " + ex.Message);
+                    return;
                 }
             }
         }
@@ -235,7 +431,38 @@ namespace Hospital
 
         private void EmployeesForm_Load(object sender, EventArgs e)
         {
+            InitializeContextMenu(); // Настройка контекстного меню
             LoadEmployeeData(); // Загрузка данных при загрузке формы
+            LoadRoles(); // Загрузка ролей
+            Pagination();
+
+            dataGridViewEmployees.Rows[0].Selected = false;
+
+            searchTextBox.Text = "Начните ввод для поиска по фио сотрудника...";
+            searchTextBox.ForeColor = Color.LightGray;
+        }
+
+        private void searchTextBox_Enter(object sender, EventArgs e)
+        {
+            if (searchTextBox.Text == "Начните ввод для поиска по фио сотрудника...")
+            {
+                searchTextBox.Text = "";
+                searchTextBox.ForeColor = Color.Black;
+            }
+        }
+
+        private void searchTextBox_Leave(object sender, EventArgs e)
+        {
+            if (searchTextBox.Text.Length == 0)
+            {
+                searchTextBox.Text = "Начните ввод для поиска по фио сотрудника...";
+                searchTextBox.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
